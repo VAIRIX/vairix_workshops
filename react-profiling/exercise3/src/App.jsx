337ecb04300f858a7f271da37a25dc69d9bdc1e9
Felix Andersson <felixww@meraki.com>
2022-03-16 09:48:33 -0300

    remove solution

--- react-profiling/exercise3/src/App.jsx
+++ react-profiling/exercise3/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState } from "react";
 import { Box } from "./components/Box";
 import "./App.css";
 
@@ -6,13 +6,8 @@ const boxKeys = Array(10)
   .fill(0)
   .map((_, i) => i);
 
-
 function App() {
   const [text, setText] = useState();
-
-  const boxes = useMemo(
-    () => boxKeys.map((key) => <Box key={key} />), []
-  );
 
   return (
     <div className="App">
@@ -24,7 +19,9 @@ function App() {
         {text}
       </section>
       <section className="box-collection">
-        {boxes}
+        {boxKeys.map((key) => (
+          <Box key={key} />
+        ))}
       </section>
     </div>
   );
